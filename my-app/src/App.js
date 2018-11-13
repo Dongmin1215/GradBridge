66cd811bfbf04d5af10c89aecfdfb833542aa87e
saelyne <lyne9492@gmail.com>
2018-11-13 19:35:35 +0900

    first trial

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Initial">
+      <img src={require('./img/graduation.png')}/>
+        <div className="Title"> 
+          GradBridge
+        </div>
+        <div>
+          Welcome! 
+          GradBridge provides information related to admission of KAIST graduate school.
+        </div>
       </div>
     );
   }
