2b784f37740baf1b0fb02361203c223383710d23
최상진 <Sangjin@choesangjin-ui-MacBook-Pro.local>
2018-11-17 13:28:59 +0900

    cs link

--- new_myapp/src/components/Landing.js
+++ new_myapp/src/components/Landing.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import * as routes from '../constants/routes';
+
 
 const LandingPage = () =>
     <div>
-        <h1>Landing Page</h1>
+        <ul>
+        	<li><Link to={routes.WIKI}>CS</Link></li>
+        	<li>ME</li>
+        	<li>EE</li>
+        	<li>CBE</li>
+    	</ul>
     </div>
 
 export default LandingPage;
