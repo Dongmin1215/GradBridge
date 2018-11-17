79c160cc756c2a67414d12a62240471f5f23e89d
최상진 <Sangjin@choesangjin-ui-MacBook-Pro.local>
2018-11-18 02:26:17 +0900

    db

--- new_myapp/src/firebase/db.js
+++ new_myapp/src/firebase/db.js
@@ -8,18 +8,6 @@ export const doCreateUser = (id, username, email) =>
     email,
   });
 
-export const doCreateEditor = (id, username, email) =>
-  db.ref('editors/${id}').set({
-  	username,
-  	email,
-  });
-
-export const doCreateVisitor = (id, username, email) =>
-  db.ref('visitors/${id}').set({
-  	username,
-  	email,
-  });
-
 export const onceGetUsers = () =>
   db.ref('users').once('value');
 
