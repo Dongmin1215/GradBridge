06dad253d21a9509183aa2ae611438bcc632e83d
Junyong Park <jyp0802@hotmail.com>
2018-11-17 13:01:19 +0900

    Changed firebase

--- new_myapp/src/firebase/firebase.js
+++ new_myapp/src/firebase/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCKPGFZa-CrSlNxqHrBjvHAe5bh4rJGIBc",
-    authDomain: "myapp-2ccee.firebaseapp.com",
-    databaseURL: "https://myapp-2ccee.firebaseio.com",
-    projectId: "myapp-2ccee",
-    storageBucket: "",
-    messagingSenderId: "1077242452126"
+    apiKey: "AIzaSyBAalLJddLr2Ak_Rp6B7vqbE2U5vqPzmnM",
+    authDomain: "gradbridge-274fe.firebaseapp.com",
+    databaseURL: "https://gradbridge-274fe.firebaseio.com",
+    projectId: "gradbridge-274fe",
+    storageBucket: "gradbridge-274fe.appspot.com",
+    messagingSenderId: "344949878320"
 };
 
 if (!firebase.apps.length) {
