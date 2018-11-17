666a8a89d42d675197d13616e4d21586ff5b6480
Dongmin Lee <theresaldm@gmail.com>
2018-11-17 14:26:21 +0900

    Add Landing page, but not linked

--- new_myapp/src/components/Landing.js
+++ new_myapp/src/components/Landing.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as routes from '../constants/routes';
-
+import './App.css';
 
 const LandingPage = () =>
-    <div>
-        <ul>
-        	<li><Link to={routes.WIKI}>CS</Link></li>
-        	<li>ME</li>
-        	<li>EE</li>
-        	<li>CBE</li>
-    	</ul>
-    </div>
+	<div className="Initial">
+			
+		<div className="Title">
+			<img id="logo" src={require('./images/logo.png')}/>
+			<h1>GradBridge</h1>
+		</div>
+		<div className="Text">
+			Welcome! <br></br>
+			GradBridge provides information related to admission of KAIST graduate school.
+			<br></br>
+			Previous applicants can collaborate to make GradBridge more reliable and awesome!s
+			<br></br>
+			<br></br>
+			To get started, click on the department that you are interested in.
+			</div>
+			<div className="Major">
+			<img id="CS" Link to={routes.WIKI} src={require('./images/CS.png')}/>
+			<img id="EE" src ={require('./images/EE.png')}/>
+			<img id="ME" src ={require('./images/ME.png')}/>
+			<img id="AE" src ={require('./images/AE.png')}/>
+		</div>
+	</div>
+
 
 export default LandingPage;
