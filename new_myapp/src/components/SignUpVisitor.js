073bf87507d604bdce2ec3d289dca6f73d390a7c
saelyne <lyne9492@gmail.com>
2018-11-17 16:08:59 +0900

    connect initial and signup-visitor

--- new_myapp/src/components/SignUpVisitor.js
+++ new_myapp/src/components/SignUpVisitor.js
@@ -80,33 +80,91 @@ class SignUpVisitorForm extends Component {
 
         return (
             <form onSubmit={this.onSubmit}>
-                <input
-                    value={username}
-                    onChange={event => this.setState(byPropKey('username', event.target.value))}
-                    type="text"
-                    placeholder="Full Name"
-                />
-                <input
-                    value={email}
-                    onChange={event => this.setState(byPropKey('email', event.target.value))}
-                    type="text"
-                    placeholder="Email Address"
-                />
-                <input
-                    value={passwordOne}
-                    onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
-                    type="password"
-                    placeholder="Password"
-                />
-                <input
-                    value={passwordTwo}
-                    onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
-                    type="password"
-                    placeholder="Password"
-                />
-                <button disabled={isInvalid} type="submit">
-                    Sign Up
-                </button>
+                <div className = "sign-up-page">
+                <div className = "Top">
+                    <div className="Top-Title">
+                        <img id="top-logo" src={require('./images/logo.png')}/>
+                        <p>GradBridge</p>
+                    </div>
+                </div>
+            
+                <div className = "sign-up-row">
+
+                <div className = "Sign-up-left">
+                    <div className = "user-info">
+                        <div className = "Sing-up-category"> 
+                            <div id = "sign-up-type">E-mail</div>
+                        </div>
+                        <div> 
+                        <input className ="sign-up-input" value={email}
+                        onChange={event => this.setState(byPropKey('email', event.target.value))}
+                        type="text"
+                        placeholder="Email Address"></input>
+                        </div>
+                    </div>
+                    <div className = "user-info">
+                        <div className = "Sing-up-category">
+                        <div id = "sign-up-type">Password</div>
+                        </div>
+                        <div>
+                        <input type="password" className ="sign-up-input" value={passwordOne}
+                        onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
+                        placeholder="Password"></input>
+                        </div>
+                    </div>
+                    <div className = "user-info">
+                        <div className = "Sing-up-category">
+                            <div id = "sign-up-type">Check Password</div>
+                        </div>
+                        <div>
+                        <input type="password" className ="sign-up-input" value={passwordTwo}
+                        onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
+                        placeholder="Password again"></input>
+                        </div>
+                    </div>
+                
+                </div>
+
+                <div className = "sign-up-right">
+
+                    <div className = "user-info">
+                        <div className = "sign-up-category-right">
+                            <div id = "sign-up-type-right">Undergraduate University</div>
+                        </div>
+                        <div>
+                            <select className = "sign-up-selection">
+                                <option value="" disabled selected>Select your Undergraduate University</option>
+                                <option value="kaist">KAIST</option>
+                                <option value="others">Others</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className = "user-info">
+                        <div className = "sign-up-category-right">
+                            <div id = "sign-up-type-right">Undergraduate Major</div>
+                        </div>
+                        <div>
+                            <select className = "sign-up-selection">
+                            <option value="" disabled selected>Select your Undergraduate Major</option>
+                            <option value="cs">Computer Science</option>
+                            <option value="ee">Electrical Engineering</option>
+                            <option value="me">Mechanical Engineering</option>
+                            <option value="ae">Aerospace Engineering</option>
+                            </select>
+                        </div>
+                    </div>
+
+                </div>
+                </div>
+                    <div className ="button-wrapper">
+                        <div className = "register-button">
+                            <button className = "sign-up-register" disabled={isInvalid} type="submit">
+                            <div id = "sign-up-btn">Register</div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
 
                 { error && <p>{error.message}</p> }
             </form>
